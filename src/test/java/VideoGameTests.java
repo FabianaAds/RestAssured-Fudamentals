8cd14a901d62703b146cef6e8ed4baed1dca9b9d
fabiana.silva <fabiana.silva@eemovel.com>
2023-01-25 22:03:50 -0300

    Create POST Method by Json format

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -3,14 +3,31 @@ import Config.VideoGameEndPoints;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.meta.When;
+
 public class VideoGameTests extends VideoGameConfig {
 
      @Test
      public void getAllGames () {
 
          RestAssured.given().when().get(VideoGameEndPoints.ALL_VIDEO_GAMES).then();
+     }
 
+     @Test
+    public  void  createNewGameByJson(){
 
+         String createBodyJson = "{\n" +
+                 "  \"category\": \"Platform\",\n" +
+                 "  \"name\": \"Mario\",\n" +
+                 "  \"rating\": \"Mature\",\n" +
+                 "  \"releaseDate\": \"2023-01-25\",\n" +
+                 "  \"reviewScore\": 89\n" +
+                 "}";
+             RestAssured.given()
+                     .body(createBodyJson)
+                     .when()
+                     .post(VideoGameEndPoints.ALL_VIDEO_GAMES)
+                     .then().assertThat().statusCode(200);
      }
 
 
