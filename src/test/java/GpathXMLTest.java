71e8062fe3dcbd2e4c0609d43cc307ba78747af1
fabiana.silva <fabiana.silva@eemovel.com>
2023-02-18 21:53:50 -0300

    getAttribute

--- src/test/java/GpathXMLTest.java
+++ src/test/java/GpathXMLTest.java
@@ -16,4 +16,15 @@ public class GpathXMLTest  extends VideoGameConfig  {
 
      }
 
+     @Test
+    public void  getAttribute() {
+         Response response =  get(VideoGameEndPoints.ALL_VIDEO_GAMES);
+         String name =  response.path("List.item.name[0].@category");
+         System.out.println("category");
+
+
+
+     }
+
+
 }
