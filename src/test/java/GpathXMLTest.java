70383de70b4698e92e4c1f96776c294105bc575c
fabiana.silva <fabiana.silva@eemovel.com>
2023-02-18 22:38:33 -0300

    getListOfXmlNodesByFindAllOnAttribute

--- src/test/java/GpathXMLTest.java
+++ src/test/java/GpathXMLTest.java
@@ -9,43 +9,50 @@ import java.util.List;
 
 import static io.restassured.RestAssured.get;
 
-public class GpathXMLTest  extends VideoGameConfig  {
+public class GpathXMLTest  extends VideoGameConfig {
 
 
-     @Test
-      public  void  getFirstGameInList(){
+    @Test
+    public void getFirstGameInList() {
 
-         Response response =  get(VideoGameEndPoints.ALL_VIDEO_GAMES);
-                 String name =  response.path("List.item.name[0]");
-         System.out.println("name");
+        Response response = get(VideoGameEndPoints.ALL_VIDEO_GAMES);
+        String name = response.path("List.item.name[0]");
+        System.out.println("name");
 
-     }
+    }
 
-     @Test
-    public void  getAttribute() {
-         Response response =  get(VideoGameEndPoints.ALL_VIDEO_GAMES);
-         String name =  response.path("List.item.name[0].@category");
-         System.out.println("category");
-
-
-
-     }
-
-     @Test
-     public  void getListOfXmlNodes() {
-           String resposeAsString = get(VideoGameEndPoints.ALL_VIDEO_GAMES).asString();
-           List<Node>allResults = XmlPath.from(resposeAsString).get(
-
-                 "List.item.findAll { element -> return element} "
-         );
-
-                System.out.println( allResults.get(2).get("name").toString());
+    @Test
+    public void getAttribute() {
+        Response response = get(VideoGameEndPoints.ALL_VIDEO_GAMES);
+        String name = response.path("List.item.name[0].@category");
+        System.out.println("category");
 
 
+    }
+
+    @Test
+    public void getListOfXmlNodes() {
+        String resposeAsString = get(VideoGameEndPoints.ALL_VIDEO_GAMES).asString();
+        List<Node> allResults = XmlPath.from(resposeAsString).get(
+
+                "List.item.findAll { element -> return element} "
+        );
+
+        System.out.println(allResults.get(2).get("name").toString());
 
 
-     }
+    }
+
+    @Test
+    public void  getListOfXmlNodesByFindAllOnAttribute() {
+
+        String resposeAsString = get(VideoGameEndPoints.ALL_VIDEO_GAMES).asString();
+        List<Node> allDrivingGames = XmlPath.from(resposeAsString).get(
+                "List.item.findAll { game ->  def  category  =  game.@category; category  ==  'Driving'} "
+        );
+
+        System.out.println(allDrivingGames.get(0).get("name").toString());
 
 
-
+    }
 }
